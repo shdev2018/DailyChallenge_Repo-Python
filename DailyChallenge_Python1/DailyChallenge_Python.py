dde8f268a93b005c913ff507dd4b4f5db18bf483
shdev2018 <shaunhall.dev@gmail.com>
2020-07-07 18:12:21 +0100

    07/07/2020 (2)

--- DailyChallenge_Python1/DailyChallenge_Python.py
+++ DailyChallenge_Python1/DailyChallenge_Python.py
@@ -2,4 +2,4 @@ from Problems import Probs
 probs = Probs()
 
 #Main
-print(probs.prob_06_07_2020([10, 15, 3, 7], 17))
+print(probs.prob_07_07_2020([1, 2, 3]))
--- DailyChallenge_Python1/Problems.py
+++ DailyChallenge_Python1/Problems.py
@@ -1,3 +1,4 @@
+from functools import reduce
 class Probs():
     """class containing problem methods"""
 
@@ -6,15 +7,12 @@ class Probs():
     def prob_07_07_2020(self, array):
         newArray = []
         for x in array:
-            total = 0
-            for y in array:
-                if y != x:
-                    if total == 0:
-                        total = y
-                    else:
-                        total += total * y
-            if True:
-                pass
+            # Remove element from list
+            temp = [w for w in array if w != x]
+            # Get product of new list and append
+            newArray.append(reduce(lambda y, z: y * z, temp, 1))
+        return newArray
+    # Completed in 00:04:22
 
     # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
     def prob_06_07_2020(self, lst, k):
@@ -27,6 +25,7 @@ class Probs():
                 j += 1
             i += 1
         return False
+    # Completed in 00:04:37
 
 
 
