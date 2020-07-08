dc4ff401ef18fcda675e59a22340d9b7b901176d
shdev2018 <shaunhall.dev@gmail.com>
2020-07-08 22:19:09 +0100

    08/07/2020

--- DailyChallenge_Python1/DailyChallenge_Python.py
+++ DailyChallenge_Python1/DailyChallenge_Python.py
@@ -2,4 +2,12 @@ from Problems import Probs
 probs = Probs()
 
 #Main
-print(probs.prob_07_07_2020([1, 2, 3]))
+
+# 08_07_2020
+probs.prob_08_07_2020()
+
+# 07_07_2020
+# print(probs.prob_07_07_2020([1, 2, 3]))
+
+# 06_07_2020
+# print(probs.prob_06_07_2020([10, 15, 3, 7]), 17)
--- DailyChallenge_Python1/Problems.py
+++ DailyChallenge_Python1/Problems.py
@@ -2,6 +2,15 @@ from functools import reduce
 class Probs():
     """class containing problem methods"""
 
+    # Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
+    # and deserialize(s), which deserializes the string back into the tree.
+    def prob_08_07_2020(self):
+        node = Node('root', Node('left', Node('left.left')), Node('right'))
+        class08_07_2020 = c08_07_2020()
+        assert class08_07_2020.deserialize(class08_07_2020.serialize(node)).left.left.val == 'left.left'
+        return node
+    # Completed in 00:18:46
+
     # Given an array of integers, return a new array such that each element at index i of the new 
     # array is the product of all the numbers in the original array except the one at i.
     def prob_07_07_2020(self, array):
@@ -28,4 +37,33 @@ class Probs():
     # Completed in 00:04:37
 
 
+# 08_07_2020
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class c08_07_2020:
+    def serialize(self, root):
+        self.vals = []
+        def encode(node):
+            if node:
+                self.vals.append(str(node.val))
+                encode(node.left)
+                encode(node.right)
+            else:
+                self.vals.append('#')
+        encode(root)
+        return ' '.join(self.vals)
+
+    def deserialize(self, data):
+        
+        def decode(vals):
+            val = next(vals)
+            if val == '#':
+                return None
+            node = Node(val, decode(vals), decode(vals))
+            return node
+        vals = iter(data.split())
+        return decode(vals)
 
